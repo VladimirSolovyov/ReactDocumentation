64daae7e0164019885a774c0bfe6d9d8a75cd20e
VladimirSolovyov <vovanchik3007@mail.ru>
2023-02-11 15:29:46 +0300

    Read about form in Rise of the state

--- src/App.js
+++ src/App.js
@@ -10,16 +10,23 @@ import Clock from './components/Clock'
 import MailBox from './components/MailBox'
 import List from './components/List'
 import NameForm from './components/NameForm'
+import Header from './components/UI/Header'
+import Br from './components/UI/Br'
+import Calculator from './components/TemperatureCalculator/Calculator'
 
-// https://ru.reactjs.org/docs/lifting-state-up.html Подъём состояния
+// https://ru.reactjs.org/docs/composition-vs-inheritance.html Композиция против наследования
 
 function App() {
 	return (
 		<div className='App'>
+			<Header title='Первые компоненты' />
 			<Test name='Vladimir' />
 			<Counter />
+			<Br />
+			<Header title='Запись данных и работа с запросами' />
 			<Cash />
 			<ListClient />
+
 			<Resume
 				fullName='Соловьёв Владимир Борисович'
 				sex='1'
@@ -27,11 +34,16 @@ function App() {
 				phone='+7-930-031-04-99'
 				mail='vovanchik3007@mail.ru'
 			/>
+			<Br />
+			<Header title='Обработка эффектов' />
 			<EffectExample></EffectExample>
 			<Clock />
 			<MailBox unreadMsg={['hi', 'js']} />
 			<List />
 			<NameForm />
+			<Br />
+			<Header title='Подъём состояния' />
+			<Calculator />
 		</div>
 	)
 }
